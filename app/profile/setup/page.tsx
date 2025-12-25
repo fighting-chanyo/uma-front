d2c8e4e983f726c537b66b65079998037959a9c5
fighting-chanyo <falcon852620@gmail.com>
2025-12-25 12:40:56 +0900

    a

--- app/profile/setup/page.tsx
+++ app/profile/setup/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { ProfileSetupForm } from "./_components/profile-setup-form";
 
 const ProfileSetupPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <ProfileSetupForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProfileSetupForm />
+      </Suspense>
     </div>
   );
 };
